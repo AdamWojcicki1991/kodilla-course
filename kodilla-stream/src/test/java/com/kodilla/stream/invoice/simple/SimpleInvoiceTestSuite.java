908b1cb09b30bed97d601a090836ea3e2f31361a
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-03-20 13:28:04 +0100

    Add Kodilla exercises: Kodilla module 7.4

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.stream.invoice.simple;
+
+import org.junit.*;
+
+public class SimpleInvoiceTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testGetValueToPay() {
+        //Given
+        SimpleInvoice invoice = new SimpleInvoice();
+        //When
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 3", 6.49), 5.0));
+        //Then
+        Assert.assertEquals(108.975, invoice.getValueToPay(), 0.001);
+    }
+}
