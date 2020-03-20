206b3d168b10005f4ba8d1ab2d62a55a6a364c28
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-03-20 17:07:07 +0100

    Add Kodilla exercises: Kodilla module 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/book/BookTestSuite.java
@@ -0,0 +1,75 @@
+package com.kodilla.stream.book;
+
+import org.junit.*;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class BookTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testGetListUsingFor() {
+        //Given
+        BookDirectory bookDirectory = new BookDirectory();
+
+        //When
+        List<Book> books = bookDirectory.getList();
+
+        //Then
+        int numberOfBooksPublicatedAfter2007 = 0;
+        for (Book book : books) {
+            if (book.getYearOfPublication() > 2007) {
+                numberOfBooksPublicatedAfter2007++;
+            }
+        }
+        Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
+    }
+
+    @Test
+    public void testGetListUsingIntStream() {
+        //Given
+        BookDirectory bookDirectory = new BookDirectory();
+
+        //When
+        List<Book> books = bookDirectory.getList();
+
+        //Then
+        int numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size())
+                .filter(n -> books.get(n).getYearOfPublication() > 2007)
+                .map(n -> 1)
+                .sum();
+        Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
+    }
+
+    @Test
+    public void testGetListUsingIntStream1() {
+        //Given
+        BookDirectory bookDirectory = new BookDirectory();
+
+        //When
+        List<Book> books = bookDirectory.getList();
+
+        //Then
+        long numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size())
+                .filter(n -> books.get(n).getYearOfPublication() > 2007)
+                .count();
+        Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
+    }
+}
