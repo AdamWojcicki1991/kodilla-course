128a159946f7c3d1065745258e10576552a0b2d7
AdamWojcicki1991 <60105724+AdamWojcicki1991@users.noreply.github.com>
2020-03-26 12:25:38 +0100

    Create ForumUser Map: Kodilla task 7.3 (#10)
    * Create ForumUser Map: Kodilla task 7.3
    
    * Create ForumUser Map: Kodilla task 7.3 rev.1
    
    * Create ForumUser Map: Kodilla task 7.3 rev.2
    
    * Create ForumUser Map: Kodilla task 7.3 rev.3
    
    * Create ForumUser Map: Kodilla task 7.3 rev.4

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMainForumUser.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMainForumUser {
+    public static void main(String[] args) {
+        Forum forum = new Forum();
+        Map<Integer, Object> resultMapOfForumUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getGender() == 'M')
+                .filter(forumUser -> Period.between(forumUser.getBirthDate(), LocalDate.now()).getYears() >= 20)
+                .filter(forumUser -> forumUser.getPublishedPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+
+        System.out.println("# elements: " + resultMapOfForumUsers.size());
+        resultMapOfForumUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+    }
+}
