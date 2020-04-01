d1f0a851221cb41b4be39b27a14ebdb0d1adf73a
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-04-01 21:48:27 +0100

    Create GAME RPS on skeleton: Kodilla task 7.7 rev.1

--- kodilla-rps/src/main/java/com/kodilla/rps/uix/SimpleUserInterface.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/uix/SimpleUserInterface.java
@@ -5,6 +5,7 @@ import com.kodilla.rps.model.Move;
 import com.kodilla.rps.model.RoundResult;
 import com.kodilla.rps.model.Statistics;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static com.kodilla.rps.model.Winner.ENEMY;
@@ -16,7 +17,7 @@ public class SimpleUserInterface implements UserInterface {
 
     @Override
     public void printMenu() {
-        System.out.println("######################################################### GAME MENU #########################################################");
+        System.out.println("############################################################### GAME MENU ###############################################################");
         System.out.println("Key 1 - play \"Stone\"");
         System.out.println("Key 2 - play \"Paper\"");
         System.out.println("Key 3 - play \"Scissors\"");
@@ -24,20 +25,77 @@ public class SimpleUserInterface implements UserInterface {
         System.out.println("Key 5 - play \"Lizard\"");
         System.out.println("Key x - End of the game, preceded by the question \"Are you sure You want to EXIT GAME?\"");
         System.out.println("Key n - Starting the game again, preceded by the question \"Are you sure You want to END current GAME and START NEW GAME?\"");
-        System.out.println("#############################################################################################################################\n");
+        System.out.println("#########################################################################################################################################\n");
     }
 
     @Override
     public void printShortMenu() {
-        System.out.println("\n###################################################### SHORT GAME MENU ######################################################");
+        System.out.println("\n############################################################ SHORT GAME MENU ############################################################");
         System.out.println("Key y - EXIT GAME OR START NEW GAME");
         System.out.println("Key n - RETURN TO PREVIUS GAME");
-        System.out.println("#############################################################################################################################\n");
+        System.out.println("#########################################################################################################################################\n");
+    }
+
+    @Override
+    public void informRound(GameDefinition definition, Move playerMove, Move enemyMove, RoundResult result) {
+        System.out.println("\n############################################################## START ROUND ##############################################################\n");
+        System.out.println(definition.getUsername() + playerMove.toString());
+        System.out.println("Computer" + enemyMove.toString());
+        System.out.println("ROUND RESULT: " + definition.getUsername() + " " + result);
+    }
+
+    @Override
+    public void informGame(GameDefinition definition, Statistics statistics) {
+        System.out.println("############################################################### END GAME ################################################################\n");
+        System.out.println(definition.getUsername() + " WINS " + statistics.getWins() + " ROUNDS" + " | " + "Computer WINS " + statistics.getLoses() + " ROUNDS");
+        System.out.println("PLAYERS HAVE " + statistics.getDraws() + " DRAWS");
+        System.out.println("GAME TAKES " + statistics.getRounds() + " ROUNDS\n");
+        if (statistics.whoWins() == PLAYER) {
+            System.out.println(definition.getUsername() + " wins gratulations !!!");
+        } else if (statistics.whoWins() == ENEMY) {
+            System.out.println("Computer wins gratulations !!!");
+        } else {
+            System.out.println(definition.getUsername() + " and Computer have a draw !!!");
+        }
+        System.out.println("\n#########################################################################################################################################\n");
+    }
+
+    @Override
+    public void printRound(int number, GameDefinition definition, Statistics statistics) {
+        System.out.println("ROUND: " + number);
+        System.out.println(definition.getUsername() + " WINS " + statistics.getWins() + " ROUNDS" + " | " + "Computer WINS " + statistics.getLoses() + " ROUNDS");
+        System.out.println("PLAYERS HAVE: " + statistics.getDraws() + " DRAWS");
+        System.out.println("WE PLAY TO " + definition.getRounds() + " WINS !\n");
+        System.out.println("############################################################### END ROUND ###############################################################\n");
+    }
+
+    @Override
+    public void printValidCharacters() {
+        System.out.println("Type a valid character '1' - ROCK, '2' - PAPER, '3' - SCISSORS, '4' - SPOCK, '5' - LIZARD or 'x' - EXIT, 'n' - NEW GAME and press ENTER: ");
+    }
+
+    @Override
+    public void printErrorMessage() {
+        System.err.println("You put invalid symbol. Please try again !");
+    }
+
+    @Override
+    public void printConfirmation(String confirmationType) {
+        if (confirmationType.equals("EXIT")) {
+            System.out.println("Are you sure You want to EXIT GAME? - type 'y' to confirm or 'n' to return to GAME\n");
+        } else if (confirmationType.equals("NEW GAME")) {
+            System.out.println("Are you sure You want to END current GAME and start NEW GAME ? - type 'y' to confirm or 'n' to return to GAME\n");
+        }
+    }
+
+    @Override
+    public void thankYou(String playerName) {
+        System.out.println("Thanks for playing " + playerName + " it was a great experiance !");
     }
 
     @Override
     public String getUserName() {
-        System.out.println("\nType Players Name and press ENTER:");
+        System.out.println("Type player name and press ENTER: ");
         return scanner.next();
     }
 
@@ -46,16 +104,19 @@ public class SimpleUserInterface implements UserInterface {
         boolean errorConfirmed = false;
         char keyboardCharacter = ' ';
         while (!errorConfirmed) {
-            System.out.println("Type a Valid Character '1' or '2' or '3' or '4' or '5' or 'x' or 'n' and press ENTER: ");
+            printValidCharacters();
             keyboardCharacter = scanner.next().charAt(0);
-            if (keyboardCharacter == '1' || keyboardCharacter == '2' || keyboardCharacter == '3' || keyboardCharacter == '4' || keyboardCharacter == '5' || keyboardCharacter == 'x' || keyboardCharacter == 'n') {
+            if (isValidCharacter(keyboardCharacter)) {
                 errorConfirmed = true;
             } else {
-                errorConfirmed = false;
-                System.err.println("You Put Invalid Character - Please Try Again");
+                printErrorMessage();
             }
         }
         return keyboardCharacter;
+    }
+
+    private boolean isValidCharacter(char keyboardCharacter) {
+        return keyboardCharacter == '1' || keyboardCharacter == '2' || keyboardCharacter == '3' || keyboardCharacter == '4' || keyboardCharacter == '5' || keyboardCharacter == 'x' || keyboardCharacter == 'n';
     }
 
     @Override
@@ -64,42 +125,16 @@ public class SimpleUserInterface implements UserInterface {
         boolean errorConfirmed = false;
         while (!errorConfirmed) {
             try {
-                System.out.println("Type a Number of Wins and press ENTER: ");
+                System.out.println("Type a number of wins and press ENTER: ");
                 result = scanner.nextInt();
                 scanner.nextLine();
                 errorConfirmed = true;
-            } catch (Exception e) {
-                errorConfirmed = false;
+            } catch (InputMismatchException e) {
                 scanner.nextLine();
-                System.err.println("You Put Invalid Character - Please Type a Number and press ENTER");
+                printErrorMessage();
             }
         }
         return result;
-    }
-
-    @Override
-    public void informRound(GameDefinition definition, Move playerMove, Move enemyMove, RoundResult result) {
-        System.out.println("\n######################################################## START ROUND ########################################################\n");
-        System.out.println(definition.getUsername() + playerMove.toString());
-        System.out.println("Computer" + enemyMove.toString());
-        System.out.println("ROUND RESULT: " + definition.getUsername() + " " + result);
-    }
-
-    @Override
-    public void informGame(GameDefinition definition, Statistics statistics) {
-
-        System.out.println("######################################################### END GAME ##########################################################\n");
-        System.out.println(definition.getUsername() + " WINS " + statistics.getWins() + " ROUNDS" + " | " + "Computer WINS " + statistics.getLoses() + " ROUNDS");
-        System.out.println("PLAYERS HAVE " + statistics.getDraws() + " DRAWS");
-        System.out.println("GAME TAKES " + statistics.getRounds() + " ROUNDS\n");
-        if (statistics.whoWins() == PLAYER) {
-            System.out.println(definition.getUsername() + " Wins Gratulations !!!");
-        } else if (statistics.whoWins() == ENEMY) {
-            System.out.println("Computer Wins Gratulations !!!");
-        } else {
-            System.out.println(definition.getUsername() + " and Computer have a DRAW !!!");
-        }
-        System.out.println("\n#############################################################################################################################\n");
     }
 
     @Override
@@ -108,13 +143,12 @@ public class SimpleUserInterface implements UserInterface {
         char keyboardCharacter = ' ';
         while (!errorConfirmed) {
             printShortMenu();
-            System.out.println("Are you sure You want to EXIT GAME? - type 'y' to confirm or 'n' to return to GAME\n");
+            printConfirmation("EXIT");
             keyboardCharacter = scanner.next().charAt(0);
             if (keyboardCharacter == 'y' || keyboardCharacter == 'n') {
                 errorConfirmed = true;
             } else {
-                errorConfirmed = false;
-                System.err.println("You Put Invalid Character - Please Try Again");
+                printErrorMessage();
             }
         }
         return keyboardCharacter == 'y';
@@ -126,29 +160,14 @@ public class SimpleUserInterface implements UserInterface {
         char keyboardCharacter = ' ';
         while (!errorConfirmed) {
             printShortMenu();
-            System.out.println("Are you sure You want to END current GAME and start NEW GAME? - type 'y' to confirm or 'n' to return to GAME\n");
+            printConfirmation("NEW GAME");
             keyboardCharacter = scanner.next().charAt(0);
             if (keyboardCharacter == 'y' || keyboardCharacter == 'n') {
                 errorConfirmed = true;
             } else {
-                errorConfirmed = false;
-                System.err.println("You Put Invalid Character - Please Try Again");
+                printErrorMessage();
             }
         }
         return keyboardCharacter == 'y';
-    }
-
-    @Override
-    public void printRound(int number, GameDefinition definition, Statistics statistics) {
-        System.out.println("ROUND: " + number);
-        System.out.println(definition.getUsername() + " WINS " + statistics.getWins() + " ROUNDS" + " | " + "Computer WINS " + statistics.getLoses() + " ROUNDS");
-        System.out.println("PLAYERS HAVE: " + statistics.getDraws() + " DRAWS");
-        System.out.println("WE PLAY TO " + definition.getRounds() + " WINS !\n");
-        System.out.println("######################################################### END ROUND #########################################################\n");
-    }
-
-    @Override
-    public void thankYou(String playerName) {
-        System.out.println("Thanks for playing " + playerName + " it was a great experiance!");
     }
 }
--- kodilla-rps/src/main/java/com/kodilla/rps/uix/UserInterface.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/uix/UserInterface.java
@@ -11,21 +11,27 @@ public interface UserInterface {
 
     void printShortMenu();
 
+    void informRound(GameDefinition definition, Move playerMove, Move enemyMove, RoundResult result);
+
+    void informGame(GameDefinition definition, Statistics statistics);
+
+    void printRound(int number, GameDefinition definition, Statistics statistics);
+
+    void printValidCharacters();
+
+    void printErrorMessage();
+
+    void printConfirmation(String confirmationType);
+
+    void thankYou(String username);
+
     String getUserName();
 
     char getValidCharacter();
 
     int getRoundCount();
 
-    void informRound(GameDefinition definition, Move playerMove, Move enemyMove, RoundResult result);
-
-    void informGame(GameDefinition definition, Statistics statistics);
-
     boolean confirmExit();
 
     boolean confirmNewGame();
-
-    void printRound(int number, GameDefinition definition, Statistics statistics);
-
-    void thankYou(String username);
 }
