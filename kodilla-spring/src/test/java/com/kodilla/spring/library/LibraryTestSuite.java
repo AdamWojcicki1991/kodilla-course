275831cee33a28ae27b79dd3571e1bc1736c25b4
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-05-10 16:45:29 +0200

    Add Kodilla exercises: Kodilla module 14.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,56 @@
+package com.kodilla.spring.library;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryTestSuite {
+    private static int testCounter;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.loadFromDb();
+        //Then
+        //do nothing
+    }
+
+    @Test
+    public void testSaveToDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.saveToDb();
+        //Then
+        //do nothing
+    }
+}
