be24e46c9558b39357e20d098118b4e3907a5c3c
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-05-22 16:56:35 +0200

    Add Kodilla exercises: Kodilla module 16.6

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("ID") + ", " +
+                    resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME"));
+            counter++;
+        }
+        resultSet.close();
+        statement.close();
+        Assert.assertEquals(5, counter);
+    }
+}
