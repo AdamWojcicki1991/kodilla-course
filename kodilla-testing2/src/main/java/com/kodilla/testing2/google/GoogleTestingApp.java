8e3ddbcc4a1ef4e6f45c9b83e410c8c971dfb4f3
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-09-03 14:44:57 +0200

    Add Kodilla exercises: Kodilla module 31.2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static com.kodilla.testing2.config.WebDriverConfig.CHROME;
+
+public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "gLFyf";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(CHROME);
+        driver.get("https://www.google.com");
+
+        WebElement searchField = driver.findElement(By.className(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
+}
